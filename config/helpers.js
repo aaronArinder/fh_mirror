aa37fe2addc64544dd0ca9f89c5f39482804db8e
Aaron Arinder <aaronarinder@gmail.com>
2019-07-29 13:46:27 -0400

    config: remove useless method from helpers.js, codestyle in prod webpack

--- config/helpers.js
+++ config/helpers.js
@@ -4,17 +4,7 @@ const path = require('path');
 const _root = path.resolve(__dirname, '..');
 
 exports.root = function (args) {
-  console.log('root', _root)
-  // comment
   args = Array.prototype.slice.call(arguments, 0);
-  // comment
-  const test = path.join.apply(path, [ _root ].concat(args));
-
-  console.log('test', test);
-  return test;
+  return path.join.apply(path, [ _root ].concat(args));
 };
 
-// likely unused
-exports.assetsPath = function (_path) {
-  return path.posix.join('static', _path);
-};
