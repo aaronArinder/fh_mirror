935e3928bb43d675c02feda51bae1ae724108632
Aaron Arinder <aaronarinder@gmail.com>
2019-08-11 17:16:30 -0400

    questionnaire: mock-data directory created   - there's a basic, dummy mock for the questionnaire

---
+++ client/src/mock-data/questionnaire.mock.js
@@ -0,0 +1,12 @@
+module.exports = JSON.stringify([
+  {
+    question_id: 1,
+    question: 'First Name',
+    type: 'intake'
+  },
+  {
+    question_id: 2,
+    question: 'Last Name',
+    type: 'intake'
+  },
+]);
