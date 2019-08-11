d12d36c880be3fea372305055ca9f5087d69a7ba
Aaron Arinder <aaronarinder@gmail.com>
2019-08-11 18:04:33 -0400

    Mock data: added direcotry, added NewUser mock questions

---
+++ client/src/mock-data/new-user.js
@@ -0,0 +1,54 @@
+module.exports = JSON.stringify(
+  [
+    {
+      question_id: 1,
+      question: 'First name',
+      required: true,
+      type: 'text',
+    },
+    {
+      question_id: 2,
+      question: 'Last name',
+      required: true,
+      type: 'text',
+    },
+    {
+      question_id: 3,
+      question: 'Email address',
+      required: true,
+      type: 'text',
+    },
+    {
+      question_id: 4,
+      question: 'Phone number',
+      required: true,
+      type: 'phone number',
+    },
+    {
+      question_id: 5,
+      question: 'Current marital status',
+      required: true,
+      options: ['Married without divorce pending', 'Married with divorce pending', 'Divorced'],
+      type: 'select',
+    },
+    {
+      question_id: 6,
+      question: 'Current case status',
+      required: true,
+      options: ['Open', 'Open but pending close', 'closed'],
+      type: 'select',
+    },
+    {
+      question_id: 7,
+      question: 'Presiding judge',
+      required: true,
+      type: 'text',
+    },
+    {
+      question_id: 8,
+      question: 'Attorny name',
+      required: true,
+      type: 'text',
+    },
+  ]
+);
