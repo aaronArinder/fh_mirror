fb7a61b36e2a52ec09f1e83e90f3f33c0a527c26
Aaron Arinder <aaronarinder@gmail.com>
2019-10-14 13:37:53 -0400

    Profile view: added view
    Currently the Profile view only has the intake_form

---
+++ src/views/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid ma-0 pa-0 fill-height>
+    <QuestionnaireModule v-if="intakeForm.length" :title="'Intake Form'" :formdata="intakeForm"/>
+  </v-container>
+</template>
+
+<script>
+import QuestionnaireModule from '@/components/QuestionnaireModule.vue'
+
+export default {
+  name: 'profile',
+  components: {
+    QuestionnaireModule,
+  },
+  created () {
+    this.$store.dispatch({ type: 'getForm', payload: { formName: 'intake_form' } });
+  },
+  computed: {
+    intakeForm () {
+      return this.$store.state.forms.intake_form;
+    },
+  },
+}
+</script>
+
