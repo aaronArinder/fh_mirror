1ffe38c06a5ac908620cde3c08315ea37117862a
Aaron Arinder <aaronarinder@gmail.com>
2019-10-14 13:36:34 -0400

    views: about/home cleanup
    Minor modificatios to both views in preparation for adding more to them.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,13 @@
 <template>
   <v-container fluid ma-0 pa-0 fill-height>
-    <QuestionnaireModule />
   </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import QuestionnaireModule from '@/components/QuestionnaireModule.vue'
 
 export default {
   name: 'home',
   components: {
-    QuestionnaireModule
-  }
+  },
 }
 </script>
